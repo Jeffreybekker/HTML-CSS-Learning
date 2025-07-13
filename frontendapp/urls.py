984dd793441c1687b7df6e9ec56a8ecdafee0dde
Jeffrey Bekker <120750708+Jeffreybekker@users.noreply.github.com>
2025-07-13 23:23:14 +0200

    Practising with text in html with css

--- frontendapp/urls.py
+++ frontendapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('buttons/', buttons, name="buttons")  # playground/buttons
+    path('buttons/', buttons, name="buttons"),  # playground/buttons
+    path('text/', text, name="text"),  # playground/buttons
 ]
--- frontendapp/views.py
+++ frontendapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def buttons(request):
     return render(request, 'buttons.html')
+
+
+def text(request):
+    return render(request, 'text.html')
